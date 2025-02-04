f952d764e1f29bc8b86f308f4fd197845c13e852
Michael Marsillo <36077677+michaelmarsillo@users.noreply.github.com>
2025-02-04 18:26:08 -0500

    all cp264 files

---
+++ mars7769_l04/src/strings_length.c
@@ -0,0 +1,25 @@
+/**
+ * -------------------------------------
+ * @file  strings_length.c
+ * Lab 4 Source Code File
+ * -------------------------------------
+ * @author Michael Marsillo, 169057769, mars7769@mylaurier.ca
+ *
+ * @version 2025-01-06
+ *
+ * -------------------------------------
+ */
+#include "functions.h"
+
+void strings_length(strings_array *data, FILE *fp_short, FILE *fp_long, int length) {
+
+    // your code here
+	for (int i = 0; i < data->lines; i++) {
+			if (strlen(data->strings[i]) < length) {
+				fprintf(fp_short, "%s\n", data->strings[i]);
+			} else {
+				fprintf(fp_long, "%s\n", data->strings[i]);
+			}
+		}
+
+}
